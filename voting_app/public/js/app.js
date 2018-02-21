f10032f887435b8d541b5abeb884efe187c3e5d0
oliver fu <ofu997@gmail.com>
2018-02-20 22:21:22 -0800

    add sorting pg 36

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -1,6 +1,8 @@
 
 class ProductList extends React.Component {
   render() {
+
+        /*
     const product=Seed.products[0];
     return (
       <div className='ui unstackable items'>
@@ -13,6 +15,29 @@ class ProductList extends React.Component {
           submitterAvatarUrl={product.submitterAvatarUrl}
           productImageUrl={product.pproductImageUrl}
         />
+      </div>
+    );
+    */
+
+    const products = Seed.products.sort((a,b) => (
+      b.votes - a.votes
+    ));
+    const productComponents = products.map((product) => (
+      <Product
+        key = {"product-" + product.id}
+        id = {product.id}
+        title={product.title}
+        description={product.description}
+        url={product.url}
+        votes={product.votes}
+        submitterAvatarUrl={product.submitterAvatarUrl}
+        productImageUrl={product.productImageUrl}
+      />
+    ));
+
+    return (
+      <div className='ui unstackable items'>
+        {productComponents}
       </div>
     );
   }
